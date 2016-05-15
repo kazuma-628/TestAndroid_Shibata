86385270ce8f85e2d9e5c5bcf042de722f09927c
Kazuma Shibata <kazuma-628@users.noreply.github.com>
2016-05-16 03:24:38 +0900

    照度センサ対応

--- app/src/main/java/com/example/kazumashibata/myapplication/MainActivity.java
+++ app/src/main/java/com/example/kazumashibata/myapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.kazumashibata.myapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,7 +15,9 @@ import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
 
     public static final String TAG = "TestApp";
@@ -25,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     public native String  testJNI();
 
 */
+    private SensorManager mSensorManager;
+    private boolean mIsSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
 //        Log.d("", testJNI());
 
         //buttonを取得
@@ -41,6 +53,53 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(clicked);
 
     }
+
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+
+        Log.i(TAG,"onResume");
+
+                /* センサーマネージャを取得する */
+        mSensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+
+        // 照度センサー
+        List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_LIGHT);
+
+        // センサマネージャへリスナーを登録(implements SensorEventListenerにより、thisで登録する)
+        if (sensors.size() > 0) {
+            Sensor sensor = sensors.get(0);
+            mIsSensor = mSensorManager.registerListener(this, sensor, 1000000);
+
+        /*
+         * int  SENSOR_DELAY_FASTEST    get sensor data as fast as possible
+         * int  SENSOR_DELAY_GAME       rate suitable for games
+         * int  SENSOR_DELAY_NORMAL     rate (default) suitable for screen orientation changes
+         * int  SENSOR_DELAY_UI         rate suitable for the user interface
+         */
+        }
+    }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // センサーの精度が変更されると呼ばれる
+        Log.i(TAG,"onAccuracyChanged");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // センサーの値が変化すると呼ばれる
+        if (event.sensor.getType() == Sensor.TYPE_LIGHT)
+        {
+
+            TextView textView3 = (TextView)findViewById(R.id.textView3);
+            textView3.setText("" + event.values[0]);
+        }
+    }
+
     private View.OnClickListener clicked = new View.OnClickListener()
     {
         public void onClick(View v)
